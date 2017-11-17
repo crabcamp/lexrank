b32ed14bee9537318d012092be990bfd1997a985
LShostenko <luka.shostenko@gmail.com>
2017-11-17 15:07:18 +0200

    Edit setuo.

--- setup.py
+++ setup.py
@@ -7,16 +7,6 @@ requirements = Path(__file__).parent / 'requirements/core.txt'
 with requirements.open(mode='rt', encoding='utf-8') as fp:
     install_requires = [line.strip() for line in fp]
 
-top_packages = [
-    'lexrank.algorithms',
-    'lexrank.assets',
-    'lexrank.mappings',
-    'lexrank.tools',
-    'lexrank.utils',
-]
-
-packages_pattern = top_packages + [p + '.*' for p in top_packages]
-
 setup(
     name='lexrank',
     maintainer='Ocean S.A.',
@@ -24,7 +14,7 @@ setup(
     version='0.0.1a',
     description='LexRank text summarization.',
     license='Closed source',
-    packages=find_packages(include=packages_pattern),
+    packages=find_packages(include=['lexrank']),
     py_modules=['settings'],
     python_requires='>=3.6.0',
     install_requires=install_requires,
